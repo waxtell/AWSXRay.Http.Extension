ba18917fb8dfc5925ba5846cba8da930258c110c
Will Axtell <waxtell@transtar1.com>
2020-03-23 14:46:11 -0400

    Lots of improvements (better determination of human readable content)

---
+++ AWSXRay.Http.Extension/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AWSXRay.Http.Extension.Extensions
+{
+    public static class ServiceProviderExtensions
+    {
+        public static ServiceProvider ActivateXRayHttpDiagnosticsLogging(this ServiceProvider provider)
+        {
+            provider.GetService<XRayHttpDiagnosticLogger>();
+
+            return provider;
+        }
+    }
+}
